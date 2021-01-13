ce7cb3033479d29e8813dd5806601d9d64ef8401
alim <saban29@mail.ru>
2021-01-13 15:13:03 +0300

    баг с кодировками решен

--- src/utils/webp-responsive-loader/index.js
+++ src/utils/webp-responsive-loader/index.js
@@ -1,11 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+const _resizeList = ["240", "320", "480", "720", "1080"];
 
-const _resizeList = ["240", "320", "480", "720", "1080"]
+const Buffer = require("buffer").Buffer;
 
-const _webpConverter = require('webp-converter');
+const _webpConverter = require("webp-converter");
 
 var _options = require("./options.json");
 
@@ -15,72 +12,59 @@ var _schemaUtils = require("schema-utils");
 
 var _path = require("path");
 
-exports.raw = void 0;
-
 module.exports = function (content) {
   const options = _loaderUtils.getOptions(this);
 
-  console.log("options=", options);
-
   _schemaUtils(_options, options);
 
-  const context = options.context || this.rootContext;
-  console.log("context=", context);
-
-  const name = options.name || '[contenthash].[ext]';
-
-  const url = _loaderUtils.interpolateName(this, name, {
-    context,
+  const url = _loaderUtils.interpolateName(this, options.name, {
+    context: this.rootContext,
     content,
-    regExp: options.regExp
+    regExp: options.regExp,
   });
   let outputPath = url;
+  console.log("url=", url);
+  console.log("content=", typeof content);
+  console.log("context=", this.context + options.name);
+  console.log("outputPath=", outputPath);
+  console.log("resourcePath=", this.resourcePath);
 
-  //Формирование массива путей для будущих webp файлов
-  const baseName = _path.basename(outputPath, _path.extname(outputPath));
+  //Формирование массива путей для будущих .webp файлов
+  const extName = _path.extname(outputPath);
+  const baseName = _path.basename(outputPath, extName);
   const dirName = _path.dirname(outputPath);
+
   let publicPathList = new Array();
+
   _resizeList.map((size) => {
-    let newPublicPath = _path.join(dirName, baseName + size + ".webp")
-    newPublicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`;
-    _webpConverter.cwebp(outputPath, newPublicPath, "-resize 0 " + size)
-    publicPathList.push(newPublicPath);
+    let newPublicPath = _path.join(dirName, baseName + size + ".webp");
+    // newPublicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`;
+    // _webpConverter.cwebp(outputPath, newPublicPath, "-resize 0 " + size);
+    // const bufbase64= btoa(content);
+    // const bufbase64 = Buffer.from(content);
+    _webpConverter
+      .buffer2webpbuffer(content, extName.slice(1), "-resize 0 " + size)
+      .then((buf) => {
+        console.log("newPublicPath=", newPublicPath);
+
+        this.emitFile(newPublicPath, buf, null);
+      });
+    // publicPathList.push(newPublicPath);
   });
-  console.log("publicPathList=", publicPathList);
+
+  // console.log("publicPathList=", publicPathList);
+
   //==============================================================
 
-  let publicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`;
-
   // Проверяем включен ли параметр emitFile
-  if (typeof options.emitFile === 'undefined' || options.emitFile) {
-    // const assetInfo = {};
+  // if (typeof options.emitFile === "undefined" || options.emitFile) {
+  //   // this.emitFile(_path.basename(outputPath), content, null);
+  //   this.emitFile(outputPath, content, null);
+  // }
 
-    // if (typeof name === 'string') {
-    //   let normalizedName = name;
-    //   const idx = normalizedName.indexOf('?');
-
-    //   if (idx >= 0) {
-    //     normalizedName = normalizedName.substr(0, idx);
-    //   }
-
-    //   const isImmutable = /\[([^:\]]+:)?(hash|contenthash)(:[^\]]+)?]/gi.test(normalizedName);
-
-    //   if (isImmutable === true) {
-    //     assetInfo.immutable = true;
-    //   }
-    // }
-
-    // Создаем ту же структуру файлов что и в источнике, если включен параметр emitFile
-    // assetInfo.sourceFilename = _path.normalize(_path.relative(this.rootContext, this.resourcePath));
-    // console.log("assetInfo=", assetInfo);
-    this.emitFile(_path.basename(outputPath), content, null);
-    // this.emitFile(outputPath, content, null);
-  }
-
-  console.log("publicPath=", publicPath);
-  // webpConverter.cwebp()
-  return `${'module.exports ='} ${publicPath};`;
+  // let publicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`;
+  // return `${"module.exports ="} ${publicPath};`;
+  return "hello";
 };
 
-const raw = true;
-exports.raw = raw;
+module.exports.raw = true;
